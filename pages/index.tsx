1503b5162a357e93c3f6271d73cf91ce60ba97d7
Paserno <paserno.x@gmail.com>
2023-07-31 23:24:51 -0400

    Home Page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 import type { NextPage } from 'next';
-import { Typography } from '@mui/material';
+import { Typography, Grid, Card, CardActionArea, CardMedia } from '@mui/material';
 
 import { ShopLayout } from '../components/layouts';
+import { initialData } from '../database/products';
 
 
 const Home: NextPage = () => {
@@ -9,6 +10,27 @@ const Home: NextPage = () => {
       <ShopLayout title={'PasernoShop - Home'} pageDescription={'Encuentra productos de Paserno'}>
         <Typography variant='h1' component='h1'>Tienda</Typography>
         <Typography variant='h2' sx={{ marginBottom: 1}}>Todo los productos</Typography>
+
+        <Grid container spacing={4}>
+          {
+            initialData.products.map(product => (
+              <Grid item xs={6} sm={4} key={ product.slug}>
+                <Card>
+                  <CardActionArea>
+                    <CardMedia 
+                      component='img'
+                      image={ `products/${product.images[0]}`}
+                      alt={ product.title }
+                    />
+                  </CardActionArea>
+                </Card>
+              </Grid>
+            ))
+          }
+        </Grid>
+
+
+
       </ShopLayout>
       )
 }
