c306fb59daf0d30b3104975b5f6b130be997a51f
Paserno <paserno.x@gmail.com>
2023-08-02 01:10:47 -0400

    Contador de productos

--- components/products/ProductSlideshow.tsx
+++ components/products/ProductSlideshow.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react';
 import { Slide } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css'
 
 import styles from './ProductSlideshow.module.css';
 
@@ -12,7 +13,7 @@ export const ProductSlideshow: FC<Props> = ({ images }) => {
   return (
     <Slide
         easing='ease'
-        duration={ 7000 }
+        duration={ 5000 }
         indicators 
     >
         {
--- pages/product/slug.tsx
+++ pages/product/slug.tsx
@@ -3,6 +3,7 @@ import { ShopLayout } from '../../components/layouts'
 import { initialData } from '../../database/products';
 import { currencyExchange } from '../../helper';
 import { ProductSlideshow } from '../../components/products';
+import { ItemCounter } from '../../components/ui';
 
 
 const product = initialData.products[0];
@@ -26,7 +27,7 @@ const ProductPage = () => {
             {/* Cantidad */}
             <Box sx={{ my: 2}}>
               <Typography variant='subtitle2'>Cantidad</Typography>
-              {/* ItemCounter */}
+              <ItemCounter />
             </Box>
 
             {/* Agregar al carrito */}
