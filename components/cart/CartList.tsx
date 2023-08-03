a6c2eaa0036c11d59d2408020dc29d76795f39fb
Paserno <paserno.x@gmail.com>
2023-08-02 20:59:19 -0400

    Mostrar productos del carrito de compra

--- components/cart/CartList.tsx
+++ components/cart/CartList.tsx
@@ -1,6 +1,9 @@
-import { } from 'react'
-import { initialData } from '../../database/products'
-import { Typography } from '@mui/material'
+import { } from 'react';
+import NextLink from 'next/link';
+import { initialData } from '../../database/products';
+import { Grid, Link, Typography, CardActionArea, CardMedia, Box, Button } from '@mui/material';
+import { ItemCounter } from '../ui/ItemCounter';
+import { currencyExchange } from '../../helper';
 
 
 const productInCart = [
@@ -14,7 +17,37 @@ export const CartList = () => {
         <>
             {
                 productInCart.map(product => (
-                    <Typography key={product.slug}>{product.title}</Typography>
+                    <Grid container spacing={2} key={product.slug} sx={{ marginBottom: 1 }}>
+                        <Grid item xs={3}>
+                            {/* TODO: Pagina del producto */}
+                            <NextLink href='product/slug' passHref legacyBehavior>
+                                <Link>
+                                    <CardActionArea>
+                                        <CardMedia
+                                            image={`products/${product.images[0]}`}
+                                            component='img'
+                                            sx={{ borderRadius: '5px' }}
+                                        />
+                                    </CardActionArea>
+                                </Link>
+                            </NextLink>
+
+                        </Grid>
+                        <Grid item xs={7}>
+                            <Box display='flex' flexDirection='column'>
+                                <Typography variant='body1'>{product.title}</Typography>
+                                <Typography variant='body1'>Talla: <strong>M</strong></Typography>
+
+                                {/* Conditional */}
+                                <ItemCounter />
+                            </Box>
+                        </Grid>
+                        <Grid item xs={2} display='flex' alignItems='center' flexDirection='column'>
+                            <Typography variant='subtitle1'>{currencyExchange(product.price)}</Typography>
+                            {/* Editable */}
+                            <Button variant='text' color='secondary'>Remover</Button>
+                        </Grid>
+                    </Grid>
                 ))
             }
         </>
