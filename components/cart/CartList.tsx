e835fe531a2f62664c85b062c9781fbbe92a26d9
Paserno <paserno.x@gmail.com>
2023-08-02 20:16:23 -0400

    Page Carrito de compra

---
+++ components/cart/CartList.tsx
@@ -0,0 +1,22 @@
+import { } from 'react'
+import { initialData } from '../../database/products'
+import { Typography } from '@mui/material'
+
+
+const productInCart = [
+    initialData.products[0],
+    initialData.products[1],
+    initialData.products[2],
+]
+
+export const CartList = () => {
+    return (
+        <>
+            {
+                productInCart.map(product => (
+                    <Typography key={product.slug}>{product.title}</Typography>
+                ))
+            }
+        </>
+    )
+}
